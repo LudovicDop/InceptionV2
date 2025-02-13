d7e8460f6c78ff05807123743d746e55be6d9a60
LudovicDop <ludovicdop@gmail.com>
2025-02-13 12:17:27 +0100

    still have an issue with the script wp-setup.sh

--- srcs/requirements/wordpress/conf/wp-setup.sh
+++ srcs/requirements/wordpress/conf/wp-setup.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-wp config create --allow-root \
-                 --dbname=$SQL_DATABASE \
-                 --dbuser=$SQL_USER \
-                 --dbpass=$SQL_PASSWORD \
+
+# Run wp-cli to configure WordPress
+# wp config create --allow-root \
+#                  --dbname=$SQL_DATABASE \
+#                  --dbuser=$SQL_USER \
+#                  --dbpass=$SQL_PASSWORD \
+#                  --dbhost=mariadb:3306 \
+#                  --path='/var/www/html' --force
+# sleep 10
+cd && wp config create --allow-root \
+                 --dbname=wordpress \
+                 --dbuser=ldoppler \
+                 --dbpass=needcoffee \
                  --dbhost=mariadb:3306 \
-                 --path='/var/www/html'
+                 --path='/var/www/html' --force
+
+echo "WordPress configuration created successfully!"
+
+sleep infinite
