c581f0812477b0b6fad6213ee41c11c1044ccc8d
ludovicdop <ldoppler@vbox.home>
2025-02-14 23:20:12 +0100

    environment variable done

--- srcs/requirements/wordpress/conf/wp-setup.sh
+++ srcs/requirements/wordpress/conf/wp-setup.sh
@@ -1,50 +1,30 @@
 #!/bin/sh
 
-# # Function to check if MariaDB is ready
-# mariadb_ready() {
-#     mysqladmin ping --host=mariadb --port=3306 --user=ldoppler --password=needcoffee > /dev/null 2>&1
-# }
-
-# # Maximum number of attempts
-# MAX_ATTEMPTS=10
-# # Initial attempt count
-# attempt=1
-
-# # Wait until MariaDB is ready or until the maximum number of attempts is reached
-# until mariadb_ready; do
-#     if [ $attempt -ge $MAX_ATTEMPTS ]; then
-#         echo "MariaDB is not ready after $attempt attempts. Exiting."
-#         exit 1
-#     fi
-#     echo "Waiting for MariaDB to be ready... (Attempt $attempt/$MAX_ATTEMPTS)"
-#     attempt=$((attempt + 1))
-#     sleep 3
-# done
 echo "\033[0;32mMariaDB is ready.\033[m\n\033[0;31mProceeding with WordPress setup...\033[m"
-
+echo "Database Name: "$SQL_DATABASE""
 sleep 10
 # Create wp-config.php
-cd /var/www/html && wp config create --allow-root \
-    --dbname=wordpress \
-    --dbuser=ldoppler \
-    --dbpass=needcoffee \
-    --dbhost=mariadb:3306 \
-    --path='/var/www/html' --force
+cd "$WP_PATH" && wp config create --allow-root \
+    --dbname="$SQL_DATABASE" \
+    --dbuser="$SQL_USER" \
+    --dbpass="$SQL_PASSWORD" \
+    --dbhost="$SQL_HOST" \
+    --path="$WP_PATH" --force
 
 sleep 10
 # Install WordPress
-cd /var/www/html && wp core install \
-    --url="https://192.168.1.76:4430/" \
-    --title="Inception - Home" \
-    --admin_user="ldoppler" \
-    --admin_password="espresso" \
-    --admin_email="ludovicdop@gmail.com" \
+cd "$WP_PATH" && wp core install \
+    --url="$WP_URL" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$WP_ADMIN_PASS" \
+    --admin_email="$WP_EMAIL" \
     --skip-email \
-    --path="/var/www/html" \
+    --path="$WP_PATH" \
     --allow-root
 
 # Install and activate the Twenty Twenty-Four theme
-wp theme install twentytwentyfour --activate --path="/var/www/html/" --allow-root
+wp theme install twentytwentyfour --activate --path="$WP_PATH" --allow-root
 
 echo "\033[0;32mWordPress configuration created successfully!\033[m"
 # Start PHP-FPM
