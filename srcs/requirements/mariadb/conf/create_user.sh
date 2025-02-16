e7445f36cfe4db13c9d1f3c0e100812b4bc307ae
ludovicdop <ldoppler@vbox.home>
2025-02-17 00:09:55 +0100

    some changement with the ports expose in the docker-compose file

--- srcs/requirements/mariadb/conf/create_user.sh
+++ srcs/requirements/mariadb/conf/create_user.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-echo "\033[0;31mCreation of the database in progress...\033[m\n"
+!/bin/sh
+echo "\033[0;31m[Creation of the database in progress...]\033[m"
 
 # Start MariaDB service
 service mariadb start
@@ -11,18 +11,27 @@ done
 
 # Execute SQL commands
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
-echo "\033[0;33mbefore\033[m\n"
+echo "\033[0;32m[Done!]\033[m\n"
 
+echo "\033[0;31m[Creation of user in progress...]\033[m"
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+
+echo "\033[0;32m[Done!]\033[m\n"
+
+echo "\033[0;31m[Creation of root user in progress...]\033[m"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
+echo "\033[0;32m[Done.]\033[m\n"
 
-echo "\033[0;35mokok\033[m\n"
 
 # Shutdown MariaDB cleanly before starting it again
 mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 
 # Start MariaDB safely
+echo "\033[1;32mFinish.\033[m\n"
 exec mysqld_safe
-echo "\033[0;32mDone.\033[m\n"
+
